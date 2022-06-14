cb5c1ac8c99c3f7c9d70fe45e8f02a8ed48b632f
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-06-14 16:06:36 -0500

    Modify comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/* Pseudo function computerDecide(){
+/* Pseudo function computerDecide: Should return a throw from the computer
     Get random number from 1 - 3
     IF random number is equal to 1:
         return rock
     IF random number is equal to 2:
         return paper...
-}*/
+*/
 function computerDecide(){
     let random_num = Math.floor(Math.random() * 3) + 1;
     switch(random_num){
@@ -16,9 +16,14 @@ function computerDecide(){
             return "Scissors";
     }
 }
-/* pseudo function playRound(playerSelection, computerSelection){
-   playerSelection_modified =  
-}
+/* pseudo function playRound: Should return the outcome of a round of rock, paper, scissors between the player and computer
+    Create a variable that will hold user prompt input
+    Create a variable that will hold the computer's choice
+    Create a variable that will hold the user's modified input
+    Output to console the computer's choice
+    Output to console the user's modified input
+    Evaluate if the player tied, won, or lost against the robot
+    Return a statement according to the outcome
  */
 function playRound(){
     let playerSelection = prompt("What will you throw?...");
@@ -37,7 +42,14 @@ function playRound(){
             return "Not a valid input. Throw a sign!";
     }
 }
-
+/* pseudo function modifySelection: returns a capitalized version of a word that it takes in as an argument
+    IF the argument is that of type string then:
+        Capitalize the first character of the string
+        Slice off the rest of the string and turn it all lowercase
+        Return a complete, concatenated string
+    IF the argument is not of the string type then:
+        Return a string that signals to the user that it's not a valid input 
+ */
 function modifySelection(selection){
     if(typeof selection === "string"){
         first_letter_capitalized = selection.charAt(0).toUpperCase();
@@ -49,7 +61,6 @@ function modifySelection(selection){
 
 function game(){
     for (let i = 0; i < 5; i++) {
-        playRound();
         console.log(playRound());
      }
 }
