fe98aa792784b3dacee7f6d49ab802f7c32343f0
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-06-14 11:45:55 -0500

    Finish playRound() function and modifySelection() helper function

--- script.js
+++ script.js
@@ -17,13 +17,39 @@ function computerDecide(){
     }
 }
 /* pseudo function playRound(playerSelection, computerSelection){
-
+   playerSelection_modified =  
 }
  */
-function playRound(playerSelection, computerSelection){
+function playRound(){
+    let playerSelection = prompt("What will you throw?...");
+    let computerSelection = computerDecide();
+    let playerSelection_modified = modifySelection(playerSelection);
+    console.log(computerSelection);
+    console.log(playerSelection_modified);
+    switch(true){
+        case playerSelection == computerSelection:
+            return "It's a tie! Throw another one out!"
+        case (playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Rock"):
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        case (playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Paper"):
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        default:
+            return "Not a valid input. Throw a sign!";
+    }
+}
 
+function modifySelection(selection){
+    if(typeof selection === "string"){
+        first_letter_capitalized = selection.charAt(0).toUpperCase();
+        rest_of_selection = selection.slice(1).toLowerCase();
+        return first_letter_capitalized + rest_of_selection;
+    }
+    return "Not a valid input. Throw a sign!";
 }
 
 function game(){
-
+    for (let i = 0; i < 5; i++) {
+        playRound();
+        console.log(playRound());
+     }
 }
