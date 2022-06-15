d4dad25b98bb75447e3f041752669739acafce19
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-06-15 13:52:05 -0500

    Fix all bugs

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let playerSelection;
+let playerSelection_modified;
+let computerSelection;
+
+let player_score = 0;
+let computer_score = 0;
+
 /* Pseudo function computerDecide: Should return a throw from the computer
     Get random number from 1 - 3
     IF random number is equal to 1:
@@ -16,7 +23,14 @@ function computerDecide(){
             return "Scissors";
     }
 }
-/* pseudo function playRound: Should return the outcome of a round of rock, paper, scissors between the player and computer
+/* pseudo function getThrows: Should get new throws from the player and the computer
+ */
+function getNewThrows(){
+    playerSelection = prompt("What will you throw?...");
+    playerSelection_modified = modifySelection(playerSelection);
+    computerSelection = computerDecide();
+}
+/* pseudo function getOutcomeOfRound: Should return a new outcome of a round of rock, paper, scissors between the player and computer
     Create a variable that will hold user prompt input
     Create a variable that will hold the computer's choice
     Create a variable that will hold the user's modified input
@@ -25,22 +39,73 @@ function computerDecide(){
     Evaluate if the player tied, won, or lost against the robot
     Return a statement according to the outcome
  */
-function playRound(){
-    let playerSelection = prompt("What will you throw?...");
-    let computerSelection = computerDecide();
-    let playerSelection_modified = modifySelection(playerSelection);
-    console.log(computerSelection);
+function getOutcomeOfRound(){
+    getNewThrows();
+
+    if (playerSelection === null){
+        return "cancelled";
+        console.log("cancelled");
+    } 
+
     console.log(playerSelection_modified);
+    console.log(computerSelection);
+    console.log(typeof playerSelection);
+
     switch(true){
-        case playerSelection == computerSelection:
-            return "It's a tie! Throw another one out!"
-        case (playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Rock"):
-            return `You win! ${playerSelection} beats ${computerSelection}`;
-        case (playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Paper"):
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        case playerSelection_modified == computerSelection:
+            return "Tie"
+        case (playerSelection_modified == "Paper" && computerSelection == "Rock" || playerSelection_modified == "Scissors" && computerSelection == "Paper" || playerSelection_modified == "Rock" && computerSelection == "Scissors"):
+            return "Win";
+        case (playerSelection_modified == "Paper" && computerSelection == "Scissors" || playerSelection_modified == "Scissors" && computerSelection == "Rock" || playerSelection_modified == "Rock" && computerSelection == "Paper"):
+            return "Lose";
         default:
-            return "Not a valid input. Throw a sign!";
+            console.log("invalid");
+            return "invalid";
     }
+}
+/* pseudo function manageRound: Prints outcome of a round as a string
+    Create an integer variable to hold the score of the player
+    Create an integer variable to hold the score of the computer
+    IF outcome of a round is a tie:
+        Log to the console that it's a tie
+    ELSE IF outcome is a win:
+        Log to the console that it's a win
+        Return a statement that shows the score of the player and the score of the computer
+    ELSE IF outcome is a loss:
+        Log to the console that it's a win
+        Return a statement that shows the score of the player and the score of the computer
+ */
+function manageRound(outcome){
+    switch(outcome){
+        case "Tie":
+            console.log("It's a tie! Throw another one out!");
+            break;
+        case "Win":
+            console.log(`You win! ${playerSelection_modified} beats ${computerSelection}`);
+            ++player_score;
+            break;
+        case "Lose":
+            console.log(`You lose! ${computerSelection} beats ${playerSelection_modified}`);
+            ++computer_score;
+            break;
+        case "invalid":
+            console.log("Invalid input. Throw a hand!");
+            break;
+        default:
+            console.log("Exiting game");
+            return "stop";
+    }
+    return `Player Score: ${player_score} Computer Score: ${computer_score}`;
+}
+/* pseudo function adjustRoundCounter: Returns an adjustment for the round counter if the outcome of the round is a tie or the player didn't give a valid input 
+    IF outcome of a round was a tie or invalid:
+        Return 1
+    ELSE:
+        Return 0
+ */
+function adjustRoundCounter(outcome){
+    if(outcome === "Tie" || outcome === "invalid") return 1;
+    return 0;
 }
 /* pseudo function modifySelection: returns a capitalized version of a word that it takes in as an argument
     IF the argument is that of type string then:
@@ -51,16 +116,19 @@ function playRound(){
         Return a string that signals to the user that it's not a valid input 
  */
 function modifySelection(selection){
-    if(typeof selection === "string"){
-        first_letter_capitalized = selection.charAt(0).toUpperCase();
-        rest_of_selection = selection.slice(1).toLowerCase();
-        return first_letter_capitalized + rest_of_selection;
-    }
-    return "Not a valid input. Throw a sign!";
+    first_letter_capitalized = selection.charAt(0).toUpperCase();
+    rest_of_selection = selection.slice(1).toLowerCase();
+    return first_letter_capitalized + rest_of_selection;
 }
 
 function game(){
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
+    for (let i = 1; i <= 5; i++) {
+        let new_outcome = getOutcomeOfRound();
+        let round_details = manageRound(new_outcome);
+        console.log(new_outcome);
+        console.log(round_details);
+        if(round_details == "stop") break;
+        i -= adjustRoundCounter(new_outcome);
+        console.log(i);
      }
 }
